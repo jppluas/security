64587f8b2616bd2e0e45203cff6407a714e61c54
BryanEstrada003 <mbestrad@espol.edu.ec>
2024-07-24 10:37:18 -0500

    completed act 3

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* 1. Carga de variables de entorno */
-require('dotenv').config()
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -23,6 +23,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Ruta para manejar el POST del formulario de inicio de sesión
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Aquí deberías realizar la consulta a tu base de datos para verificar las credenciales
+  // Supongamos que tienes una función que verifica las credenciales llamada checkCredentials
+  checkCredentials(username, password, (err, isValid) => {
+    if (err) {
+      // Manejo de errores
+      return res.redirect('/');
+    }
+
+    if (isValid) {
+      res.redirect('/users');
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+// Función ficticia para verificar credenciales
+function checkCredentials(username, password, callback) {
+  // Aquí deberías hacer la lógica real para consultar la base de datos
+  // Por ahora, asumimos que cualquier usuario y contraseña son válidos
+  if (username === 'user' && password === 'pass') {
+    callback(null, true);
+  } else {
+    callback(null, false);
+  }
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
